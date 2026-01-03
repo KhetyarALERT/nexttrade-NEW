8a956045808d8ae9f898d472e43425dfe133b79d
ruyaa <admin@ruyaacapital.com>
2026-01-03 17:42:30 +0400

    chore: add '@' alias to vite and include path import

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
+import path from 'path'
 import base44 from "@base44/vite-plugin"
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      // Allow imports like "@/App.jsx" to resolve to ./src/App.jsx
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   plugins: [
     base44({
       // Support for legacy code that imports the base44 SDK with @/integrations, @/entities, etc.
